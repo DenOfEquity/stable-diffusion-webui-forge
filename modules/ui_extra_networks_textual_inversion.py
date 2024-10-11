bacbb85a415f9fa464cf91c6246d26c91dbf8954
DenOfEquity <166248528+DenOfEquity@users.noreply.github.com>
2024-10-12 00:53:19 +0100

    enable sorting by version
    version auto-detected from shape: should be reliable
    Setting `lora_filter_disabled` applies to embeddings and checkpoints too. Maybe should be renamed, description updated, and moved into shared_options.py

--- modules/ui_extra_networks_textual_inversion.py
+++ modules/ui_extra_networks_textual_inversion.py
@@ -4,6 +4,15 @@ import modules.textual_inversion.textual_inversion
 from modules.shared import cmd_opts
 from modules import ui_extra_networks, shared
 from modules.ui_extra_networks import quote_js
+import enum
+
+class SdVersion(enum.Enum): #   same as in lora.network
+    Unknown = 1
+    SD1 = 2
+    SD2 = 3
+    SDXL = 4
+#    SD3 = 5
+    Flux = 6
 
 
 embedding_db = modules.textual_inversion.textual_inversion.EmbeddingDatabase()
@@ -28,7 +37,7 @@ class ExtraNetworksPageTextualInversion(ui_extra_networks.ExtraNetworksPage):
         search_terms = [self.search_terms_from_path(embedding.filename)]
         if embedding.hash:
             search_terms.append(embedding.hash)
-        return {
+        item = {
             "name": name,
             "filename": embedding.filename,
             "shorthash": embedding.shorthash,
@@ -39,6 +48,20 @@ class ExtraNetworksPageTextualInversion(ui_extra_networks.ExtraNetworksPage):
             "local_preview": f"{path}.preview.{shared.opts.samples_format}",
             "sort_keys": {'default': index, **self.get_sort_keys(embedding.filename)},
         }
+
+        match getattr(embedding, 'shape', 0):
+            case 768:
+                sd_version = SdVersion.SD1
+            case 1024:
+                sd_version = SdVersion.SD2
+            case 2048:
+                sd_version = SdVersion.SDXL
+            case _:
+                sd_version = SdVersion.Unknown
+
+        item["sd_version_str"] = str(sd_version)
+        
+        return item
 
     def list_items(self):
         # instantiate a list to protect against concurrent modification
