b12ddce06b74b2dd5ddbc3a42f046f4b1f556091
DenOfEquity <166248528+DenOfEquity@users.noreply.github.com>
2024-10-14 19:44:50 +0100

    batch sub-tabs consistent filename

--- modules/img2img.py
+++ modules/img2img.py
@@ -122,6 +122,7 @@ def process_batch(p, input, output_dir, inpaint_mask_dir, args, to_scale=False, 
         if output_dir:
             p.outpath_samples = output_dir
             p.override_settings['save_to_dirs'] = False
+        if opts.img2img_batch_use_original_name:
             p.override_settings['save_images_replace_action'] = "Add number suffix"
             if p.n_iter > 1 or p.batch_size > 1:
                 p.override_settings['samples_filename_pattern'] = f'{image_path.stem}-[generation_number]'
